b66f5f22c616593dde3eaf9e5ba72210eb9b72ad
shakil <36266736+sXakil@users.noreply.github.com>
2019-01-14 01:00:22 +0600

    first and final commit

---
+++ app.js
@@ -0,0 +1,13 @@
+$('#next').click(function() {
+	$.ajax({
+		method: 'GET',
+		url: 'https://api.thecatapi.com/v1/images/search',
+		success: function(data) {
+			$('#image').attr('src', data[0].url);
+		},
+		error: function(xhr) {
+			var error = $.parseJSON(xhr.responseText);
+			$('#error').html(error.status + ' ' + error.message);
+		}
+	})
+})
