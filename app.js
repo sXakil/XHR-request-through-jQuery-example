e8520d225c974450beb394cc24417b9c4ae28546
Shakil Ahmed <36266736+sXakil@users.noreply.github.com>
2019-01-26 21:52:32 +0600

    few UI adjustment included
    the images will now fade in dynamically according to their loading time

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ $(document).ready(function() {
 	$('#next').click(refersh);
 })
 
+const url = 'https://api.thecatapi.com/v1/images/search';
+let downloadingImage = new Image();
 function refersh() {
+	$("#image").fadeTo("slow", 0.2);
 	$.ajax({
 		method: 'GET',
-		url: 'https://api.thecatapi.com/v1/images/search',
-		success: function(data) {
-			$('#image').attr('src', data[0].url);
+		url,
+		success(data) {
+			downloadingImage.src = data[0].url;
+			downloadingImage.onload = function(){
+    			$('#image').attr('src', this.src).fadeTo("slow", 1);
+			};
 		},
-		error: function(xhr) {
+		error(xhr) {
 			var error = $.parseJSON(xhr.responseText);
 			$('#error').html(error.status + ' ' + error.message);
 		}
-	})
+	});
 }
